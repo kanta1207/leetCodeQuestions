d9eadb523f0a38b861c72fcf2b685cef7e0b6b8e
kanta1207 <kan0712livepool@gmail.com>
2024-03-16 22:30:58 -0700

    new: solve problem No.27

---
+++ problems/27/index.ts
@@ -0,0 +1,13 @@
+/**
+ *
+ * @see {@link https://leetcode.com/problems/remove-element/}
+ */
+function removeElement(nums: number[], val: number): number {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+}
