2ed52babf0e7c584fca8e54826cd81f3523fa94b
kanta1207 <kan0712livepool@gmail.com>
2024-04-14 23:54:33 -0700

    solved

---
+++ problems/neetCode/array&hashing/1. Two Sum.ts
@@ -0,0 +1,20 @@
+/**
+ *
+ * 1 Two Sum
+ * https://leetcode.com/problems/two-sum/
+ * Easy
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ */
+function twoSum(nums: number[], target: number): number[] {
+  const hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+    if (hash[diff] !== undefined) {
+      return [hash[diff], i];
+    }
+    hash[nums[i]] = i;
+  }
+
+  return [];
+}
