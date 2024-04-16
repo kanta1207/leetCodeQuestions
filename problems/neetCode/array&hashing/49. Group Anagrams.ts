f466c12d4b01099ed46b6caecbc69a854f946f96
kanta1207 <kan0712livepool@gmail.com>
2024-04-15 23:40:02 -0700

    add problem

---
+++ problems/neetCode/array&hashing/49. Group Anagrams.ts
@@ -0,0 +1,8 @@
+/**
+ * 49. Group Anagrams
+ * https://leetcode.com/problems/group-anagrams/
+ * Medium
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ */
+
+function groupAnagrams(strs: string[]): string[][] {}
