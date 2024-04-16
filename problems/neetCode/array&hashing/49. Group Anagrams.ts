4de4eeb29628e840870d739fae9076698d122b57
kanta1207 <kan0712livepool@gmail.com>
2024-04-15 23:49:06 -0700

    solved

--- problems/neetCode/array&hashing/49. Group Anagrams.ts
+++ problems/neetCode/array&hashing/49. Group Anagrams.ts
@@ -8,6 +8,7 @@
 function groupAnagrams(strs: string[]): string[][] {
   const hash = {};
   for (let str of strs) {
+    // Turn the string into an array of characters, sort it by alphabetical order, and join it back into a string.
     const sortedStr = str.split('').sort().join('');
     if (!hash[sortedStr]) {
       hash[sortedStr] = [str];
@@ -18,3 +19,6 @@ function groupAnagrams(strs: string[]): string[][] {
 
   return Object.values(hash);
 }
+
+// Time complexity: O(n * m * log(m)) where n is the number of strings in the input array and m is the length of the longest string.
+// Space complexity: O(n * m)
