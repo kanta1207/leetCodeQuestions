c9e68f856460f5bec43f0c271e582be01c68a3da
kanta1207 <kan0712livepool@gmail.com>
2024-04-15 23:42:08 -0700

    solve

--- problems/neetCode/array&hashing/49. Group Anagrams.ts
+++ problems/neetCode/array&hashing/49. Group Anagrams.ts
@@ -5,4 +5,16 @@
  * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
  */
 
-function groupAnagrams(strs: string[]): string[][] {}
+function groupAnagrams(strs: string[]): string[][] {
+  const hash = {};
+  for (let str of strs) {
+    const sortedStr = str.split('').sort().join('');
+    if (!hash[sortedStr]) {
+      hash[sortedStr] = [str];
+    } else {
+      hash[sortedStr].push(str);
+    }
+  }
+
+  return Object.values(hash);
+}
