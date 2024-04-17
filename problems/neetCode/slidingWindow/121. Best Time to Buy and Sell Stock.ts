691238d0fa85c8eda587a0ecb91885ac06377176
kanta1207 <kan0712livepool@gmail.com>
2024-04-17 13:07:11 -0700

    add sliding window

--- problems/neetCode/slidingWindow/121. Best Time to Buy and Sell Stock.ts
+++ problems/neetCode/slidingWindow/121. Best Time to Buy and Sell Stock.ts
@@ -7,15 +7,33 @@
 
 function maxProfit(prices: number[]): number {
   let minPrice = prices[0];
-  let maxProfit = 0;
+  let profit = 0;
 
-  for (let i = 1; i < prices.length; i++) {
-    if (prices[i] < minPrice) {
-      minPrice = prices[i];
+  for (let price of prices) {
+    if (price < minPrice) {
+      minPrice = price;
     } else {
-      maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+      profit = Math.max(profit, price - minPrice);
     }
   }
+  return profit;
+}
+// Time complexity: O(n)
+// Space complexity: O(1)
 
-  return maxProfit;
+// Another solution: Use Sliding Window
+function maxProfit2(prices: number[]): number {
+  let left = 0;
+  let right = 1;
+  let profit = 0;
+
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      profit = Math.max(profit, prices[right] - prices[left]);
+    } else {
+      left = right;
+    }
+    right++;
+  }
+  return profit;
 }
