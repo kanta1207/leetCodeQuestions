672782c8778a191108ffcaf9813e7ad75a578cac
kanta1207 <kan0712livepool@gmail.com>
2024-04-21 11:55:05 -0700

    solve with better approach

--- problems/meetup/linkedList/21. Merge Two Sorted Lists.ts
+++ problems/meetup/linkedList/21. Merge Two Sorted Lists.ts
@@ -1,45 +1,43 @@
-//  Definition for singly-linked list.
-class ListNode {
-  val: number;
-  next: ListNode | null;
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
-  }
-}
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
 
 function mergeTwoLists(
-  list1: ListNode | null,
-  list2: ListNode | null
+  l1: ListNode | null,
+  l2: ListNode | null
 ): ListNode | null {
-  if (!list1) return list2;
-  if (!list2) return list1;
+  const dummyHead = new ListNode(); // Dummy head node
+  let current = dummyHead; // Pointer to the current node in the merged list
 
-  const shouldStartFromList1 = list1.val <= list2.val;
-  const mergedList = new ListNode(shouldStartFromList1 ? list1.val : list2.val);
-  let currentNode = mergedList;
-  if (shouldStartFromList1) {
-    list1 = list1.next;
-  } else {
-    list2 = list2.next;
-  }
-  while (list1 !== null && list2 !== null) {
-    if (list1.val <= list2.val) {
-      currentNode.next = new ListNode(list1.val);
-      currentNode = currentNode.next;
-
-      list1 = list1.next;
+  // Iterate through both lists simultaneously
+  while (l1 !== null && l2 !== null) {
+    // Compare the values of the current nodes in both lists
+    if (l1.val < l2.val) {
+      // Append the smaller node from list1 to the merged list
+      current.next = l1;
+      // Move to the next node in list1
+      l1 = l1.next;
     } else {
-      currentNode.next = new ListNode(list2.val);
-      currentNode = currentNode.next;
-
-      list2 = list2.next;
+      // Append the smaller node from list2 to the merged list
+      current.next = l2;
+      // Move to the next node in list2
+      l2 = l2.next;
     }
+    // Move to the next node in the merged list
+    current = current.next;
   }
-  if (list1 !== null) {
-    currentNode.next = list1;
-  } else if (list2 !== null) {
-    currentNode.next = list2;
-  }
-  return mergedList;
+
+  // Append the remaining nodes from list1 or list2 to the merged list
+  current.next = l1 !== null ? l1 : l2;
+
+  // Return the head of the merged list (excluding the dummy head)
+  return dummyHead.next;
 }
